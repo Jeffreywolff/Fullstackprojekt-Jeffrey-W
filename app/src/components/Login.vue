c4c7b25894540ea4c6e2a2924bd5b28f7a9e121a
Jeffreywolff <jeffrey.wolff@elev.ga.ntig.se>
2021-04-23 14:14:37 +0200

    Initial commit

---
+++ app/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Login">
+        <h1>Hello Hugo</h1>
+        <p>{{hugo}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    props: {
+        hugo: String,
+    }
+    
+}
+</script>
+
+
+<style scoped>
+
+</style>
